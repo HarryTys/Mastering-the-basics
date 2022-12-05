3cc354d44e9d5efa5d461d8bb5a5f3fd87fe38b0
HarryTys <113461358+HarryTys@users.noreply.github.com>
2022-12-05 13:30:40 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace C_Sharp_Controlled_Iteration
+{
+    class C_Sharp_Controlled_Iteration
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What year were your born in?");
+            int birthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hi " + name + "," + "\nI think you are " + (2022 - birthYear) + ",");
+            if (2022 - birthYear < 18 && 2022 - birthYear > 12) { Console.WriteLine("That means your are a teenager."); }
+            else if (2022 - birthYear < 12) { Console.WriteLine("That means you are a child."); }
+            else if (2022 - birthYear >= 18) { Console.WriteLine("That means you are an adult."); }
+            Console.ReadLine();
+        }
+    }
+}
